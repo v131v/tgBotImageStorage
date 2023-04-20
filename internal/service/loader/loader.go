e0df95798b648149856b8b0d4774f69513760eb7
v131v <jsjsjs87@mail.ru>
2023-04-20 21:07:52 +0300

    feat: add loader service

---
+++ internal/service/loader/loader.go
@@ -0,0 +1,31 @@
+package loaderservice
+
+import "os"
+
+type LoaderService struct {
+	storagePath string
+}
+
+func New(storagePath string) *LoaderService {
+	return &LoaderService{storagePath}
+}
+
+func (ls *LoaderService) Get(dirName string) (filePaths []string, err error) {
+	dirPath := ls.storagePath + dirName
+	dirFile, err := os.Open(dirPath)
+	if err != nil {
+		return
+	}
+
+	defer dirFile.Close()
+
+	filesInfo, err := dirFile.ReadDir(-1)
+	if err != nil {
+		return
+	}
+
+	for _, fileInfo := range filesInfo {
+		filePaths = append(filePaths, fileInfo.Name())
+	}
+	return
+}
