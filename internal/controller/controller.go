8967fa5305dd3f68067f40e2e753e4d1cc06deb6
v131v <jsjsjs87@mail.ru>
2023-04-20 19:38:41 +0300

    feat: add controller

---
+++ internal/controller/controller.go
@@ -0,0 +1,37 @@
+package controller
+
+import (
+	"tgbotimgstor/internal/loader"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type Controller struct {
+	loader loader.LoadService
+}
+
+func New() Controller {
+	return Controller{}
+}
+
+func (c Controller) HandleMessage(msg tgbotapi.Message) (*tgbotapi.MediaGroupConfig, *tgbotapi.MessageConfig) {
+	groupName := msg.CommandArguments()
+
+	switch msg.Command() {
+	case "load":
+		fileNames, err := loader.Load(groupName)
+		if err != nil {
+			errMsg := tgbotapi.NewMessage(msg.Chat.ID, string(err))
+			return nil, &errMsg
+		}
+
+		photos := []any{}
+
+		for _, filePath := range fileNames {
+			photos = append(photos, tgbotapi.NewInputMediaPhoto(tgbotapi.FilePath(filePath)))
+		}
+
+		group := tgbotapi.NewMediaGroup(msg.Chat.ID, photos)
+		return &group, nil
+	}
+}
