65983ea820d70313289eb8381fe4298b9f4647ea
v131v <jsjsjs87@mail.ru>
2023-04-20 23:26:42 +0300

    feat: add main (runner)

--- cmd/tgbotimgstor/main.go
+++ cmd/tgbotimgstor/main.go
@@ -1,10 +1,31 @@
-package tgbotimgstor
+package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"tgbotimgstor/internal/controller/commands"
+	"tgbotimgstor/internal/server"
+	"tgbotimgstor/internal/service/loader"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Started")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	storagePath := os.Getenv("STORAGE_PATH")
+	botToken := os.Getenv("BOT_TOKEN")
+
+	ls := loader.New(storagePath)
+	ctrl := commands.New(ls)
+	serv, err := server.New(ctrl, botToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print("Started")
+	serv.Run()
 }
