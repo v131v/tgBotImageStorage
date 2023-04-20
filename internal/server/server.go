71d4eab6669706618a2e0c17aea8e980d5b70e8f
v131v <jsjsjs87@mail.ru>
2023-04-20 20:19:58 +0300

    feat: add server

---
+++ internal/server/server.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"tgbotimgstor/internal/controller"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type Server struct {
+	bot  *tgbotapi.BotAPI
+	ctrl *controller.Controller
+}
+
+func New(ctrl *controller.Controller, token string) (s Server, err error) {
+	s.bot, err = tgbotapi.NewBotAPI(token)
+	if err != nil {
+		return
+	}
+	s.ctrl = ctrl
+	return
+}
+
+func (s *Server) Run() {
+
+	updCfg := tgbotapi.NewUpdate(0)
+	updCfg.Timeout = 30
+
+	updates := s.bot.GetUpdatesChan(updCfg)
+
+	for update := range updates {
+		if update.Message == nil || !update.Message.IsCommand() {
+			continue
+		}
+
+		resp := s.ctrl.HandleMessage(update.Message)
+		s.bot.Send(resp)
+	}
+}
